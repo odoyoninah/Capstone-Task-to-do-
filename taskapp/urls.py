7dfa8636617b15c3f8a2144b31dae539cc451efa
odoyonina <odoyoninah@gmail.com>
2022-06-23 11:08:37 +0300

    create login and signup files

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -1,3 +1,14 @@
-from django.urls import path
-from . import viewset
+from django.conf import settings
+from django.urls import path,include
+from django.contrib import admin
+from . import views
+from django.conf.urls.static import static
 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('account/', include('django.contrib.auth.urls')), 
+]
