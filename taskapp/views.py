f72284e8c506f076a8022a86c66b18ae7ae3d058
odoyonina <odoyoninah@gmail.com>
2022-06-23 11:01:30 +0300

    create views imports

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+import email
+from multiprocessing import context
+from django.forms import Form
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+#handle all the status code responses.
+
+def index(request):
+    return render(request,'index.html')
